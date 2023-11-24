43762c6076e953408e359d89e472c499b2b20db2
jsb <tjdqls2021@gmail.com>
2023-11-24 12:33:34 +0900

    다음 라우터 설치

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import HomeLayout from "./layout/HomeLayout";
+import React from "react";
+import RestaurantForm from "./page/restaurant/restaurantForm";
+import RestaurantList from "./page/restaurant/restaurantList";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<HomeLayout />}>
+      <Route path="restaurantList" element={<RestaurantList />} />
+      <Route path="restaurantForm" element={<RestaurantForm />} />
+    </Route>,
+  ),
+);
 
 function App() {
-  return (
-    <div>
-        hello
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
