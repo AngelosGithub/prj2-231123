9a95531f065bac5c505c442eb8303ccfc4fd7f2d
jsb <tjdqls2021@gmail.com>
2023-11-27 14:58:54 +0900

    카카오맵 수정중

--- src/App.js
+++ src/App.js
@@ -5,20 +5,51 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import HomeLayout from "./layout/HomeLayout";
-import React from "react";
-import RestaurantForm from "./page/restaurant/restaurantForm";
-import RestaurantList from "./page/restaurant/restaurantList";
+import React, { useEffect, useState } from "react";
+import RestaurantForm from "./page/restaurant/RestaurantForm";
+import RestaurantList from "./page/restaurant/RestaurantList";
+
+import { RestaurantView } from "./page/restaurant/RestaurantView";
+import { RestaurantEdit } from "./page/restaurant/RestaurantEdit";
+import ImageSlide from "./component/ImageSlide";
+import KakaoMap from "./component/KakaoMap";
+import axios from "axios";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<HomeLayout />}>
       <Route path="restaurantList" element={<RestaurantList />} />
+
+      <Route path="restaurant/view/:no" element={<RestaurantView />} />
       <Route path="restaurantForm" element={<RestaurantForm />} />
+      <Route path="restaurant/edit/:no" element={<RestaurantEdit />} />
+      <Route path="imgae" element={<ImageSlide />} />
     </Route>,
   ),
 );
 
 function App() {
+  // const [mapKey, setMapKey] = useState(null);
+  //
+  // useEffect(() => {
+  //   axios
+  //     .get("/api/restaurant/map/key")
+  //     .then((response) => setMapKey(response.data));
+  // }, []);
+  //
+  // if (mapKey === null) {
+  //   return null;
+  // }
+  //
+  // // insert kakao map script tag
+  // let scriptTag = document.getElementById("kakaMapApiScriptTag");
+  // if (!scriptTag) {
+  //   scriptTag = document.createElement("script");
+  //   scriptTag.id = "kakaMapApiScriptTag";
+  //   scriptTag.type = "text/javascript";
+  //   scriptTag.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${mapKey}&libraries=services,clusterer,drawing&autoload=false`;
+  //   document.querySelector("title").before(scriptTag);
+  // }
   return <RouterProvider router={router} />;
 }
 
