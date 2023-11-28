139a5b740f3e77f4c680713e2df238ea3b7d6995
Angelo <ljy6037@gmail.com>
2023-11-28 11:33:08 +0900

    231128 리뷰리스트 불러오기 완료

--- src/pages/review/ReviewList.js
+++ src/pages/review/ReviewList.js
@@ -1,11 +1,58 @@
-import { Box, Button, Heading } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Heading,
+  Spinner,
+  Table,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export function ReviewList() {
+  const [reviewList, setReviewList] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("/api/review/list")
+      .then((response) => setReviewList(response.data));
+  }, []);
+
   const navigate = useNavigate();
   return (
     <Box>
-      <Heading>게시물 보기</Heading>
+      <Heading>리뷰 보기</Heading>
+      <Box>
+        <Table>
+          <Thead>
+            <Tr>
+              <Th>글번호</Th>
+              <Th>제목</Th>
+              <Th>작성자</Th>
+              <Th>작성시간</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {reviewList === null ? (
+              <Spinner />
+            ) : (
+              reviewList.map((review) => (
+                <Tr>
+                  <Td>{review.no}</Td>
+                  <Td>{review.title}</Td>
+                  <Td>{review.writer}</Td>
+                  <Td>{review.inserted}</Td>
+                </Tr>
+              ))
+            )}
+          </Tbody>
+        </Table>
+      </Box>
       <Button onClick={() => navigate("/write")}>리뷰 쓰기</Button>
     </Box>
   );
