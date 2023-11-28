0b2ad6c4271986882492aef2068fc87d6e6115f5
Angelo <ljy6037@gmail.com>
2023-11-28 12:42:57 +0900

    231128 리뷰 삭제기능 추가중

--- src/pages/review/ReviewView.js
+++ src/pages/review/ReviewView.js
@@ -1,10 +1,12 @@
-import { Box, Heading, Spinner, Text } from "@chakra-ui/react";
+import { Box, Button, Heading, Spinner, Text } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
 
 export function ReviewView() {
   const [review, setReview] = useState(null);
+
+  const navigate = useNavigate();
 
   const { no } = useParams();
 
@@ -18,6 +20,19 @@ export function ReviewView() {
     return <Spinner />;
   }
 
+  function handleDelete() {
+    axios
+      .delete("/api/review/remove" + no)
+      .then((response) => {
+        console.log("delete");
+        navigate("/");
+      })
+      .catch((error) => {
+        console.log("error");
+      })
+      .finally(() => console.log("done"));
+  }
+
   return (
     <Box>
       <Heading>리뷰 보기</Heading>
@@ -26,6 +41,8 @@ export function ReviewView() {
       <Text>내용 : {review.content}</Text>
       <Text>작성자 : {review.writer}</Text>
       <Text>작성일 : {review.inserted}</Text>
+      <Button>수정</Button>
+      <Button onClick={handleDelete}>삭제</Button>
     </Box>
   );
 }
