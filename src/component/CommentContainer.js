e7c35abe39279e7eccbc30b51747de1058dd9723
Angelo <ljy6037@gmail.com>
2023-12-05 15:43:00 +0900

    231205 댓글 삭제시 모달로 확인

--- src/component/CommentContainer.js
+++ src/component/CommentContainer.js
@@ -7,13 +7,21 @@ import {
   Flex,
   Heading,
   Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
   Stack,
   StackDivider,
   Text,
   Textarea,
+  useDisclosure,
 } from "@chakra-ui/react";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { DeleteIcon, EditIcon } from "@chakra-ui/icons";
 
 function CommentForm({ reviewId, isSubmitting, onSubmit }) {
@@ -36,7 +44,7 @@ function CommentForm({ reviewId, isSubmitting, onSubmit }) {
   );
 }
 
-function CommentList({ commentList, onDelete, isSubmitting }) {
+function CommentList({ commentList, onDeleteModal, isSubmitting }) {
   return (
     <Card>
       <CardHeader>
@@ -61,7 +69,7 @@ function CommentList({ commentList, onDelete, isSubmitting }) {
                   </Button>
                   <Button
                     isDisabled={isSubmitting}
-                    onClick={() => onDelete(comment.no)}
+                    onClick={() => onDeleteModal(comment.no)}
                     size={"xs"}
                     colorScheme="red"
                   >
@@ -82,6 +90,13 @@ export function CommentContainer({ reviewId }) {
 
   // 댓글 요청상태를 알아내는 공통 state
   const [isSubmitting, setIsSubmitting] = useState(false);
+  // 댓글 데이터를 배열 형태로 응답받음
+  const [commentList, setCommentList] = useState([]);
+  // 댓글 키 값을 state에 저장
+  const [no, setNo] = useState(0);
+
+  // 모달 컨트롤
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   // 댓글 쓰기 함수
   function handleSubmit(comment) {
@@ -95,16 +110,17 @@ export function CommentContainer({ reviewId }) {
       //요청이 끝나면 다시 false
       .finally(() => setIsSubmitting(false));
   }
-  function handleDelete(no) {
+  function handleDelete() {
     // 삭제버튼 클릭시 key값을 가져오는 지 확인
     // console.log(no + "Comment Deleted");
 
     setIsSubmitting(true);
     // 삭제 요청 보내기
-    axios.delete("/api/comment/" + no).finally(() => setIsSubmitting(false));
+    axios.delete("/api/comment/" + no).finally(() => {
+      setIsSubmitting(false);
+      onClose();
+    });
   }
-
-  const [commentList, setCommentList] = useState([]);
 
   // 댓글 리스트 불러오는 함수
   useEffect(() => {
@@ -121,6 +137,12 @@ export function CommentContainer({ reviewId }) {
     }
   }, [isSubmitting]);
 
+  function handleDeleteModal(no) {
+    // id를 어딘가 저장
+    setNo(no);
+    // >> 모달 열기
+    onOpen();
+  }
   return (
     <Box>
       {/* Form에서 submit 되었을때 List가 다시 랜더링 되어야하기 때문에
@@ -134,8 +156,29 @@ export function CommentContainer({ reviewId }) {
         reviewId={reviewId}
         isSubmitting={isSubmitting}
         commentList={commentList}
-        onDelete={handleDelete}
+        onDeleteModal={handleDeleteModal}
       />
+
+      {/* 삭제 모달 */}
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>삭제 확인</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>삭제 하시겠습니까?</ModalBody>
+
+          <ModalFooter>
+            <Button onClick={onClose}>닫기</Button>
+            <Button
+              isDisabled={isSubmitting}
+              onClick={handleDelete}
+              colorScheme="red"
+            >
+              삭제
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 }
